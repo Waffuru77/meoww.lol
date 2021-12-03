cbfc2eaad48d25420dd659017dd14d60aedf6a7c
Waffuru77 <79039135+Waffuru77@users.noreply.github.com>
2021-12-03 23:29:06 +0200

    new website
    download link woooo

---
+++ script/main.js
@@ -0,0 +1,11 @@
+function MenuHome() {
+    window.location.href = "./index.html";
+}
+
+function MenuDownloads() {
+    window.location.href = "./downloads.html";
+}
+
+function FNaClink() {
+    window.location.href = "https://drive.google.com/file/d/1tnND5Vn-aWCJYAYUUGXUt_FQgD9bHlYD/view?usp=sharing";
+}
